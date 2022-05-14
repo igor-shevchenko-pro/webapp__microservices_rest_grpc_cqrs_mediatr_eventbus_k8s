cc56d24e4910864bb089fa77d9c6e70aab017b49
Igor Shevchenko <igor.shevchenko.pro@gmail.com>
2022-05-14 16:50:48 +0300

    Created BaseApiController and PlatformController

---
+++ CommandCenter/CommandCenter.API/Controllers/Base/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandCenter.API.Controllers.Base
+{
+    [ApiController]
+    public abstract class BaseApiController : ControllerBase
+    {
+        // Define here any common logic
+
+        protected virtual string GetUserId() => string.Empty;
+
+        protected virtual string GetLanguageId() => string.Empty;
+    }
+}
