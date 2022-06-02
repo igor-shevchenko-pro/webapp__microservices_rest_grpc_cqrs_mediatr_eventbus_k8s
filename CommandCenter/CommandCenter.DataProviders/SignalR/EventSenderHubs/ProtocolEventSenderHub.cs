80212faff7a75aa010c972061a4ffc7f2c1d118b
Igor Shevchenko <igor.shevchenko.pro@gmail.com>
2022-06-02 12:24:15 +0300

    Created DataProviders and replaced SignalRHubs into it

---
+++ CommandCenter/CommandCenter.DataProviders/SignalR/EventSenderHubs/ProtocolEventSenderHub.cs
@@ -0,0 +1,33 @@
+﻿using CommandCenter.Core.Interfaces.EventSenderHubs;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CommandCenter.BLL.EventSenderHubs
+{
+    /// <summary>
+    /// SignalR ProtocolEventSenderHub
+    /// </summary>
+    public class ProtocolEventSenderHub : Hub, IProtocolEventSenderHub
+    {
+        private readonly string _hubMethod;
+        private readonly IHubContext<ProtocolEventSenderHub> _hubContext;
+        private readonly ILogger<ProtocolEventSenderHub> _logger;
+
+        /// <summary>
+        /// Constructor of ProtocolEventSenderHub
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="hubContext"></param>
+        /// <param name="logger"></param>
+        public ProtocolEventSenderHub(
+            IConfiguration configuration,
+            IHubContext<ProtocolEventSenderHub> hubContext,
+            ILogger<ProtocolEventSenderHub> logger)
+        {
+            _hubMethod = configuration["SocketAPI:Protocol:Method"];
+            _hubContext = hubContext;
+            _logger = logger;
+        }
+    }
+}
